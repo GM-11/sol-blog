d89d11384555728df429643f16aefbea23d4444d
GM-11 <mathuyrgopal@gmail.com>
2023-12-13 19:07:18 +0530

    showing all blogs

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,31 +1,20 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import "./page.css";
 import {
-  Connection,
-  PublicKey,
-  clusterApiUrl,
-  ConfirmOptions,
-} from "@solana/web3.js";
-import {
-  Program,
-  AnchorProvider,
-  web3,
-  utils,
-  // BN,
-} from "@project-serum/anchor";
-import idl from "./idl.json";
+  getConnection,
+  getProvider,
+  getProgram,
+  blog,
+  baseAccount,
+} from "@/utils/utils";
 
-import { Buffer } from "buffer";
-
-window.Buffer = Buffer;
-
-const network = clusterApiUrl("devnet");
-const programID = new PublicKey(idl.metadata.address);
-const opts = {
-  preflightCommitment: "processed" as ConfirmOptions,
-};
+import { BN } from "@project-serum/anchor";
 
 function Home() {
   const [address, setAddress] = useState(null);
+  const [blogs, setBlogs] = useState<[]>([]);
 
   async function checkSolanaWallet() {
     if (window.solana) {
@@ -39,16 +28,69 @@ function Home() {
     }
   }
 
-  async function getProvider() {
-    const connection = new Connection(network, opts.preflightCommitment);
-    const provider = new AnchorProvider(
-      connection,
-      window.solana,
-      opts.preflightCommitment
-    );
+  async function connectSolanaWallet() {
+    if (window.solana) {
+      console.log("Phantom detected");
+      const res = await window.solana.connect({
+        onlyIfTrusted: true,
+      });
+      setAddress(res.publicKey.toString());
+    } else {
+      alert("Please install phantom wallet");
+    }
   }
 
-  return <div></div>;
+  async function getAllBlogs() {
+    const program = await getProgram();
+    const account = (await program.account.blogs.fetch(
+      baseAccount.publicKey
+    )) as {
+      totalBlogs: BN;
+      blogsList: [];
+    };
+
+    setBlogs(account.blogsList);
+  }
+
+  useEffect(() => {
+    async function onLoad() {
+      await checkSolanaWallet();
+    }
+    window.addEventListener("load", onLoad);
+    return function () {
+      return window.removeEventListener("load", onLoad);
+    };
+  }, []);
+
+  return (
+    <div>
+      {address ? (
+        <>
+          <p>{address}</p>
+          <Link href="/makeNewBlog" style={{ textDecoration: "none" }}>
+            Make new blog
+          </Link>
+          <br />
+          <button onClick={getAllBlogs}>Get blogs</button>
+          <br />
+          {blogs.map(function (blog: blog) {
+            return (
+              <div className="blog-card" key={Number(blog.timestamp)}>
+                <p>{blog.author.toString()}</p>
+                <h2>{blog.title}</h2>
+                <p>{blog.content}</p>
+                <Link href={`/getBlog/${blog.timestamp}`}>See this blog</Link>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <>
+          <button onClick={connectSolanaWallet}>Connect Wallet</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Home;
