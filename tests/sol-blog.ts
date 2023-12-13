d3680e5cb1b31b176586ffa89425bf1b1c532ae6
GM-11 <mathuyrgopal@gmail.com>
2023-12-13 16:52:09 +0530

    added (and failed) tests. Now shipping into production 😁

--- tests/sol-blog.ts
+++ tests/sol-blog.ts
@@ -1,6 +1,9 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolBlog } from "../target/types/sol_blog";
+import { BN, web3 } from "@project-serum/anchor"
+import { SystemProgram } from "@solana/web3.js";
+
 
 describe("sol-blog", () => {
   // Configure the client to use the local cluster.
@@ -9,8 +12,27 @@ describe("sol-blog", () => {
   const program = anchor.workspace.SolBlog as Program<SolBlog>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const baseAccount = web3.Keypair.generate()
+    const user = web3.Keypair.generate()
+    const tx = await program.methods.initialize()
+      .accounts({
+        baseAccount: baseAccount.publicKey, user: user.publicKey, systemProgram: SystemProgram.programId
+      })
+      .signers([baseAccount, user])
+      .rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("adding blog", async () => {
+    const timestamp = Date.now()
+    const baseAccount = web3.Keypair.generate()
+    const user = web3.Keypair.generate()
+    const tx = await program.methods.addBlog("this is blog contnent", "i wrote this blog", new BN(timestamp)).accounts({
+      baseAccount: baseAccount.publicKey, user: user.publicKey
+    }).signers([baseAccount, user]).rpc()
+
+    const blogs = await program.account.blogs.fetch(baseAccount.publicKey)
+    console.log(`Total blogs: ${blogs.totalBlogs}`)
+    console.log(`Blogs: ${blogs.blogsList}`)
+  })
 });
