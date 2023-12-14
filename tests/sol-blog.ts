e6dcd6ea2dc6a670f7b337a59529fe3aad2c7451
GM-11 <mathuyrgopal@gmail.com>
2023-12-14 14:29:20 +0530

    tests are passing

--- tests/sol-blog.ts
+++ tests/sol-blog.ts
@@ -1,5 +1,5 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
 import { SolBlog } from "../target/types/sol_blog";
 import { BN, web3 } from "@project-serum/anchor"
 import { SystemProgram } from "@solana/web3.js";
@@ -7,29 +7,27 @@ import { SystemProgram } from "@solana/web3.js";
 
 describe("sol-blog", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
+  const baseAccount = web3.Keypair.generate()
   const program = anchor.workspace.SolBlog as Program<SolBlog>;
 
   it("Is initialized!", async () => {
-    const baseAccount = web3.Keypair.generate()
-    const user = web3.Keypair.generate()
     const tx = await program.methods.initialize()
       .accounts({
-        baseAccount: baseAccount.publicKey, user: user.publicKey, systemProgram: SystemProgram.programId
+        baseAccount: baseAccount.publicKey, user: provider.wallet.publicKey, systemProgram: SystemProgram.programId
       })
-      .signers([baseAccount, user])
+      .signers([baseAccount])
       .rpc();
     console.log("Your transaction signature", tx);
   });
 
   it("adding blog", async () => {
     const timestamp = Date.now()
-    const baseAccount = web3.Keypair.generate()
-    const user = web3.Keypair.generate()
     const tx = await program.methods.addBlog("this is blog contnent", "i wrote this blog", new BN(timestamp)).accounts({
-      baseAccount: baseAccount.publicKey, user: user.publicKey
-    }).signers([baseAccount, user]).rpc()
+      baseAccount: baseAccount.publicKey, user: provider.wallet.publicKey
+    }).rpc()
 
     const blogs = await program.account.blogs.fetch(baseAccount.publicKey)
     console.log(`Total blogs: ${blogs.totalBlogs}`)
